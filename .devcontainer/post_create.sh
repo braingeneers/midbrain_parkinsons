3fa6b24bd79d7cfee656e80198b00a1492f1202a
Matthew Elliott <melliot1@ucsc.edu>
2023-10-05 05:37:03 +0000

    removed code from post_create, to add it in later

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -10,19 +10,22 @@ python3 -m pip install diptest
 # This script should be run from the root of the repository
 
 # Define the base directory path
-base_dir="${HOME}/data/"
+#base_dir="${HOME}/data/"
+
+
+#base_dir="/workspaces/human_hippocampus/data/ephys"
 
 # Create the base directory
-mkdir -p "$base_dir"
+#mkdir -p "$base_dir"
 
 # Define an array of dataset names
-dataset_names=("2023-04-02-e-hc328_unperturbed/derived/" "2022-10-20-e-/derived/" "2022-11-02-e-Hc11.1-chip16753/derived" "2023-05-10-e-hc52_18790_unperturbed/derived")
+#dataset_names=("2023-04-02-e-hc328_unperturbed/derived/" "2022-10-20-e-/derived/" "2022-11-02-e-Hc11.1-chip16753/derived" "2023-05-10-e-hc52_18790_unperturbed/derived")
 
 # Loop through the dataset names
-for sub_dir in "${dataset_names[@]}"; do
+#for sub_dir in "${dataset_names[@]}"; do
   # Create directories
-  mkdir -p "$base_dir/$sub_dir"
+  #mkdir -p "$base_dir/$sub_dir"
 
   # Download data from S3
-  aws --endpoint https://s3-west.nrp-nautilus.io s3 cp "s3://braingeneers/ephys/$sub_dir" "$base_dir/$sub_dir" --recursive --no-sign-request
-done
+  #aws --endpoint https://s3-west.nrp-nautilus.io s3 cp "s3://braingeneers/ephys/$sub_dir" "$base_dir/$sub_dir" --recursive --no-sign-request
+#done
