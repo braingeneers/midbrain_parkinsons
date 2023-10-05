d13c5e8aa7dbaf904cbdcbb146839f765cdf35d9
Matthew Elliott <melliot1@ucsc.edu>
2023-10-05 06:29:51 +0000

    added curated data, formatted ephys data location

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -9,23 +9,18 @@ python3 -m pip install diptest
 # This script will download the datasets from S3 and place them in the correct directory structure
 # This script should be run from the root of the repository
 
-# Define the base directory path
-#base_dir="${HOME}/data/"
-
-
-#base_dir="/workspaces/human_hippocampus/data/ephys"
-
-# Create the base directory
-#mkdir -p "$base_dir"
-
-# Define an array of dataset names
-#dataset_names=("2023-04-02-e-hc328_unperturbed/derived/" "2022-10-20-e-/derived/" "2022-11-02-e-Hc11.1-chip16753/derived" "2023-05-10-e-hc52_18790_unperturbed/derived")
-
+# Define and create base directory path
+base_dir="/workspaces/human_hippocampus/data/ephys"
+mkdir -p "$base_dir"
+# Download all the spikesorted datasets by name
+dataset_names=("2023-04-02-e-hc328_unperturbed/derived/" "2022-10-20-e-/derived/" "2022-11-02-e-Hc11.1-chip16753/derived" "2023-05-10-e-hc52_18790_unperturbed/derived")
 # Loop through the dataset names
-#for sub_dir in "${dataset_names[@]}"; do
+for sub_dir in "${dataset_names[@]}"; do
   # Create directories
-  #mkdir -p "$base_dir/$sub_dir"
-
+  mkdir -p "$base_dir/$sub_dir"
   # Download data from S3
-  #aws --endpoint https://s3-west.nrp-nautilus.io s3 cp "s3://braingeneers/ephys/$sub_dir" "$base_dir/$sub_dir" --recursive --no-sign-request
-#done
+  aws --endpoint https://s3-west.nrp-nautilus.io s3 cp "s3://braingeneers/ephys/$sub_dir" "$base_dir/$sub_dir" --recursive --no-sign-request
+done
+
+# Create curated versions of the data
+python3 /workspaces/human_hippocampus/src/curate_data.py
