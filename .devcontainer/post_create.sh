40049df6a3c9762a5344012b1c09996350a8dc45
Matthew Elliott <melliot1@ucsc.edu>
2023-10-10 22:07:13 +0000

    added surys lfp code

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -3,6 +3,7 @@
 # Installs `human_hip` as local library without resolving dependencies (--no-deps)
 python3 -m pip install -e /workspaces/human_hippocampus --no-deps
 python3 -m pip install diptest
+python3 -m pip install PyWavelets
 
 # Loop through the dataset names to download them for S3
 for data in "2023-04-02-e-hc328_unperturbed/derived/"  "2022-11-02-e-Hc11.1-chip16753/derived/" "2023-05-10-e-hc52_18790_unperturbed/derived/"
@@ -10,7 +11,7 @@ for data in "2023-04-02-e-hc328_unperturbed/derived/"  "2022-11-02-e-Hc11.1-chip
 do
   # Create directories and download data from aws into them
   mkdir -p "/workspaces/human_hippocampus/data/ephys/$data"
-  aws --endpoint https://s3-west.nrp-nautilus.io s3 cp "s3://braingeneers/ephys/$data" "/workspaces/human_hippocampus/data/ephys/$data" --recursive --no-sign-request
+  aws --endpoint https://s3.braingeneers.gi.ucsc.edu s3 cp "s3://braingeneers/ephys/$data" "/workspaces/human_hippocampus/data/ephys/$data" --recursive --no-sign-request
 done
 
 # Create curated versions of the data
