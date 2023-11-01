f912c86d93ad24394fbfe0627f503d9c3b94002f
Matthew Elliott <melliot1@ucsc.edu>
2023-11-01 19:40:33 +0000

    added dataset and pyhton package

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -4,9 +4,10 @@
 python3 -m pip install -e /workspaces/human_hippocampus --no-deps
 python3 -m pip install diptest
 python3 -m pip install PyWavelets
+python3 -m pip install spkit
 
 # Loop through the dataset names to download them for S3
-for data in "2023-04-02-e-hc328_unperturbed/derived/"  "2022-11-02-e-Hc11.1-chip16753/derived/" "2023-05-10-e-hc52_18790_unperturbed/derived/"
+for data in "2023-04-02-e-hc328_unperturbed/derived/"  "2022-11-02-e-Hc11.1-chip16753/derived/" "2023-05-10-e-hc52_18790_unperturbed/derived/" "2023-05-10-e-hc52_18790/derived/"
 # note- we exclude a potential data, "2022-10-20-e-/derived/", infant dentate, feel free to download and analyze it! 
 do
   # Create directories and download data from aws into them
