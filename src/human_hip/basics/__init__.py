5aef2ac0647217ea548e1e4d0942f03e8e056357
Matthew Elliott <melliot1@ucsc.edu>
2023-11-22 06:37:16 +0000

    changes from before

--- src/human_hip/basics/__init__.py
+++
@@ -1,8 +0,0 @@
-from .read_phy_files import read_phy_files
-from .plots import raster_fancy_plot, raster_plot
-
-__all__ = ["read_phy_files", "raster_fancy_plot", "raster_plot"]
-
-
-#from ..example import example_func # example of looking one directory up for a command
-
