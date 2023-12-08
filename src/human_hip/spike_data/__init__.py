af84efe61508d7b2a0f5a3a1a25962bf531723be
Matthew Elliott <melliot1@ucsc.edu>
2023-12-08 05:39:20 +0000

    added instant firing rate tutorial

--- src/human_hip/spike_data/__init__.py
+++ src/human_hip/spike_data/__init__.py
@@ -1,8 +1,9 @@
 from .read_phy_files import read_phy_files
 from .raster_plots import raster_fancy_plot, raster_plot
 from .summary_text import summary_text, summary_UUID
+from .instant_firing_rate import instant_firing_rate
 
-__all__ = ["read_phy_files", "raster_fancy_plot", "raster_plot", "summary_text", "summary_UUID"]
+__all__ = ["read_phy_files", "raster_fancy_plot", "raster_plot", "summary_text", "summary_UUID", "instant_firing_rate"]
 
 
 #from ..example import example_func # example of looking one directory up for a command
