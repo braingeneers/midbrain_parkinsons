fecfd6200bee34cbe8996959b80129f32de89168
Matthew Elliott <melliot1@ucsc.edu>
2025-02-26 20:45:34 +0000

    delete dev environoments

--- dev/elliott/spike_data/Sury/latencies/__init__.py
+++
@@ -1,9 +0,0 @@
-from ..spike_data.plot_latencies import vector_plot
-from ..spike_data.latencies import getLatencies, getLatencyTimes
-#from .curation_plots import plot_curation
-
-__all__ = ["vector_plot", "getLatencies", "getLatencyTimes"]
-
-
-#from ..example import example_func # example of looking one directory up for a command
-
